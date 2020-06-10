4b4eb0d576d991f24e4503c3b0e8f9db4a142ec5
hugo-hontans <hugohontans@gmail.com>
2020-06-10 18:31:39 +0200

    clean todo-move

--- src/lib/todo-move/todo-move.js
+++ src/lib/todo-move/todo-move.js
@@ -4,32 +4,22 @@ import Button from "react-bootstrap/Button";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 export function TodoMove(props) {
-  if (props.position === "first") {
-    return (
-      <div className="group">
-        <Button className="arrow" size="sm" variant="secondary" onClick={props.downTodo}>
-          <FaArrowDown />
-        </Button>
-      </div>
-    );
-  } else if (props.position === "last") {
-    return (
-      <div className="group">
-        <Button className="arrow" size="sm" variant="secondary" onClick={props.upTodo}>
-          <FaArrowUp />
-        </Button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="group">
-        <Button className="arrow" variant="secondary" size="sm" onClick={props.upTodo}>
-          <FaArrowUp />
-        </Button>
-        <Button className="arrow" size="sm" variant="secondary" onClick={props.downTodo}>
-          <FaArrowDown />
-        </Button>
-      </div>
-    );
-  }
+  return (
+    <div className="group">
+      {
+        props.position !== 'first'
+        ? (<Button className="arrow" size="sm" variant="secondary" onClick={props.upTodo}>
+            <FaArrowUp />
+          </Button>)
+        : null
+      }
+      {
+        props.position !== 'last'
+        ? (<Button className="arrow" size="sm" variant="secondary" onClick={props.downTodo}>
+            <FaArrowDown />
+          </Button>)
+        : null
+      }
+    </div>
+  )
 }
