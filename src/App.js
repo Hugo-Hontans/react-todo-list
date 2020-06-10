bf3ee2be4aaa1982f6406f05ac66367416eb76ed
hugo-hontans <hugohontans@gmail.com>
2020-06-10 19:01:34 +0200

    add routing with react router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { TodoList } from "./lib/todo-list/todo-list.js";
 import { TodoAdd } from "./lib/todo-add/todo-add";
 import { TodoSend } from "./lib/todo-send/todo-send";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
@@ -41,21 +47,43 @@ class App extends React.Component {
 
   render() {
     return (
-      <section className="container text-center">
-        <h1 className="title">TODO LIST</h1>
-        <TodoList
-          className="row"
-          todos={this.state.todos}
-          onChange={(todos) => this.onChange(todos)}
-          removeTodo={(index) => this.removeTodo(index)}
-          upTodo={(index) => this.moveTodo(index, -1)}
-          downTodo={(index) => this.moveTodo(index, +1)}
-        />
-        <TodoAdd addTodo={() => this.addTodo()} />
-        <article className="send">
-          <TodoSend sendTodos={() => this.sendTodos()}></TodoSend>
-        </article>
-      </section>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <section className="container text-center">
+              <h1>Home</h1>
+            </section>
+          </Route>
+          <Route path="/todo">
+            <section className="container text-center">
+              <h1 className="title">TODO LIST</h1>
+              <TodoList
+                className="row"
+                todos={this.state.todos}
+                onChange={(todos) => this.onChange(todos)}
+                removeTodo={(index) => this.removeTodo(index)}
+                upTodo={(index) => this.moveTodo(index, -1)}
+                downTodo={(index) => this.moveTodo(index, +1)}
+              />
+              <TodoAdd addTodo={() => this.addTodo()} />
+              <article className="send">
+                <TodoSend sendTodos={() => this.sendTodos()}></TodoSend>
+              </article>
+            </section>
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
