6e3ee75bfcf2495bbcebbcd5aa1e7364a9164e95
hugo-hontans <hugohontans@gmail.com>
2020-06-18 21:43:40 +0200

    fix margin of add button

--- client/src/lib/todo-add/todo-add.js
+++ client/src/lib/todo-add/todo-add.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 
 export function TodoAdd(props) {
     return (
-        <Button className="add-button" variant="primary" onClick={props.addTodo}>
+        <Button variant="primary" onClick={props.addTodo}>
             Add
         </Button>
     ); 
